56edf8f9481a4b30c14210370e69fc100712315d
ahmadtaim450-lang <ahmadtaim450@gmail.com>
2026-06-13 03:55:56 +0200

    Fix vercel.json rewrite to not break static files

--- src/sections/Founders.tsx
+++ src/sections/Founders.tsx
@@ -18,7 +18,7 @@ const founders: FounderData[] = [
   {
     name: 'Abed Istampooli',
     role: 'CTO & Co-Founder',
-    humanSrc: '/founders_photo/abed-human.jpg',
+    humanSrc: '/founders_photo/abed-human.png',
     robotSrc: '/founders_photo/abed-robot.jpg',
   },
 ]
